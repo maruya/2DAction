4a300da3d4eb9445ebd227475628621c1f08b1d4
maruya <paradim_not_guilty@yahoo.co.jp>
2014-12-16 18:07:41 +0900

    test commit
    test

---
+++ New Unity Project 1/Assets/Scripts/Play/Stage1/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	private float speed = 0f ;			// 実際に代入する速度
+	private const float Speed = 2.0f ;	// プレイヤーは固定の速度で移動する
+	private bool IsRight = true ;		// キャラクターの向きを判断するフラグ
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		// 移動の方向に速度を代入
+		if (Input.GetKey ("left")) speed = -Speed;
+		else if (Input.GetKey ("right"))speed = Speed;
+		else speed = 0f;
+	
+	}
+
+	void FixedUpdate()
+	{
+		// 速度を制限
+		float h = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (speed, this.rigidbody2D.velocity.y);
+
+		// キャラクターの向きを制御
+		if (h > 0 && ! this.IsRight || h < 0 && this.IsRight) 
+		{
+			this.IsRight = (h > 0) ;
+			float scale = Mathf.Abs(this.transform.localScale.x) ;
+			this.transform.localScale = new Vector3(( this.IsRight ? scale : -scale ), this.transform.localScale.y, this.transform.localScale.z) ;
+		}
+
+	}
+}
---
+++ New Unity Project 1/Assets/Scripts/Title/SelectMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectMenu : MonoBehaviour {
+	
+	void Start () {
+		Debug.Log ("test");
+	}
+
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		// Start	or	Exit
+		if(GUI.Button(new Rect(120,180,200,20), "Start")) Application.LoadLevel("StageSelect") ;
+		if (GUI.Button (new Rect (120, 220, 200, 20), "Exit")) Application.Quit ();
+	}
+
+}
