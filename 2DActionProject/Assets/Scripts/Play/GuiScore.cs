ee089e55862063737dd5ba42f827e9a6b25c2a22
rash02 <paradim_not_guilty@yahoo.co.jp>
2014-12-24 01:24:40 +0900

    アイテムの追加と実装

--- 2DActionProject/Assets/Scripts/Play/GuiScore.cs
+++ 2DActionProject/Assets/Scripts/Play/GuiScore.cs
@@ -6,7 +6,7 @@ public class GuiScore : MonoBehaviour {
     // GUIの表示
     [ExecuteInEditMode()]
 
-    private int score = 100;
+    private int score = 0;
 
 	void Start () {
 	
@@ -17,9 +17,10 @@ public class GuiScore : MonoBehaviour {
         // スコアの更新
         //PlayerMove player = GameObject.Find("player").GetComponent<PlayerMove>();
         //score = player.GetStatus().Score;
+        int drawScore = GameObject.Find("player").GetComponent<BaseCharacter>().score;
 
         // 描画する
-        this.guiText.text = "Score_" + score;
+        this.guiText.text = "Score_" + drawScore;
 	
 	}
 }
---
+++ 2DActionProject/Assets/Scripts/Play/Item/ItemMain.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMain : MonoBehaviour {
+
+    public int point = 0;   // アイテムの持つポイント
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void CreateDrawScore()
+    {
+        string drawScore = point.ToString();
+        float addX = 0;								// スコアの描画の重なりを防止
+
+        // スコアをプレハブから生成
+        foreach (var score in drawScore)
+        {
+            string serif = "Prefabs/ScoreNumbers/" + score;
+            Vector3 pos = new Vector3(transform.position.x + addX, transform.position.y, transform.position.z);
+            Instantiate(Resources.Load(serif), pos, this.transform.rotation);
+            addX += 0.5f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2d)
+    {
+        // プレイヤーにスコアを渡し消滅する
+        if (collider2d.name == "player")
+        {
+            GameObject.Find("player").GetComponent<BaseCharacter>().score += point;
+            CreateDrawScore();
+            Destroy(gameObject);
+        }
+    }
+}
