708ecf06525ec6c3b29815116282ef4abcf91d07
maruya <paradim_not_guilty@yahoo.co.jp>
2014-12-24 17:55:47 +0900

    ステージセレクトシーンに仮でステージ名,条件,スコアのウィンドウを表示

---
+++ 2DActionProject/Assets/Scripts/StageSelect/ElectIcon/ElectIconInformation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectIconInformation : MonoBehaviour {
+
+	public string information ;		// ステージ名,条件,スコアのどれかが入る
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+		this.guiText.text = "" + information;
+	}
+}
