4b94b4669fa3893189f393773f44b33df8e5a066
maruya <paradim_not_guilty@yahoo.co.jp>
2014-12-24 16:05:24 +0900

    ステージ２にギミックを実装

--- 2DActionProject/Assets/Scripts/Play/Stage2/AntsNestMove.cs
+++ 2DActionProject/Assets/Scripts/Play/Stage2/AntsNestMove.cs
@@ -6,17 +6,19 @@ public class AntsNestMove : MonoBehaviour {
 	public GameObject origianlAnt ;					// 蟻のデータ
 	private Vector2 AntSize = new Vector2(1f,1.5f);	// 敵のサイズ範囲
 	private const int MAX_ANTS = 2 ;				// 巣から生成される最大数(親の子ノードに座標Objが１つあるため内部の処理でこの値に+1している)
-	private const float INTERVAL_TIME = 5f;			// 再出現までのインターバル
+	private const float INTERVAL_TIME = 12f;		// 再出現までのインターバル
 	private float prevTime ;						// 前回生成を行った時間
+	private bool isObjectNestEixt ;					// 出口にオブジェクトがあるか判定
+
 
 	void Start () {
 	
 		// 初期化
 		prevTime = Time.time;
+		isObjectNestEixt = false;
 
 		// 初回に１匹生成する
 		CreateAnt ();
-
 	}
 
 
@@ -38,6 +40,8 @@ public class AntsNestMove : MonoBehaviour {
 
 	void CreateCheckAnts()
 	{
+		if (isObjectNestEixt) return;
+
 		// + 1はMAX_ANTSのコメントを参照
 		int num = transform.childCount;
 		if (num >= MAX_ANTS + 1) return;
@@ -58,7 +62,27 @@ public class AntsNestMove : MonoBehaviour {
 		// 生成するか判断して動く関数
 		CreateCheckAnts ();
 	}
+
+
+	void OnTriggerEnter2D(Collider2D collider2d)
+	{
+		if (collider2d.transform.tag == "Object") isObjectNestEixt = true;
+	}
+
+	void OnTriggerExit2D(Collider2D collider2d)
+	{
+		// 出口にオブジェクトがあるか判定
+		if (collider2d.transform.tag == "Object") isObjectNestEixt = false;
+	}
 }
+
+
+
+
+
+
+
+
 
 
 
